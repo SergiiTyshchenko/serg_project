3110824a6024afb15c0e252a409e52b83fc11238
Sergii Tyshchenko <sergii_tyhchenko@epam.com>
2016-04-13 15:15:42 +0300

    #5 Telco accelerator config added

---
+++ telco/custom/telcotrail/telcotrailinitialdata/src/de/hybris/telcotrail/initialdata/constants/TelcotrailInitialDataConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2015 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ * 
+ *  
+ */
+package de.hybris.telcotrail.initialdata.constants;
+
+import de.hybris.telcotrail.initialdata.constants.GeneratedTelcotrailInitialDataConstants;
+
+/**
+ * Global class for all TelcotrailInitialData constants.
+ */
+public class TelcotrailInitialDataConstants extends GeneratedTelcotrailInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "telcotrailinitialdata";
+
+	private TelcotrailInitialDataConstants()
+	{
+		//empty
+	}
+}
